6a26f99397b745b9af48a3c8702710d90dd0ae0f
new-village <kazuki.niimura@gmail.com>
2023-01-16 15:13:46 +0000

    update license

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     description='cnparser is a parser library of Corporate Number Publication Site data.',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    license = 'Apache Software License'
+    license = 'Apache License 2.0'
     install_requires=['requests', 'bs4', 'kanjize==1.2.0', 'normalize-japanese-addresses==0.0.5'],
     packages=find_packages(),
     package_data={'': ['config/*.json']},
