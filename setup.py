c8672c5b09d471246d914d205e50fb5b09877c62
new-village <kazuki.niimura@gmail.com>
2024-03-30 15:03:32 +0900

    update file_id list

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='cnparser',
-    version='1.2.0',
+    version='1.2.1',
     author='new-village',
     url='https://github.com/new-village/cnparser',
     description='cnparser is a parser library of Corporate Number Publication Site data.',
