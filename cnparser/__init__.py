2ea679f168fcff76775ce88880dac27e8dbe0f23
new-village <kazuki.niimura@gmail.com>
2024-05-12 15:12:12 +0900

    add data anaysis notebook

--- cnparser/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Corporate Number Parser Library
-~~~~~~~~~~~~~~~~~~~~~
-cnparser is a simple scraping library for Corporate Number Publication Site
-"""
-from cnparser.load import bulk_load, bulk_enrich
