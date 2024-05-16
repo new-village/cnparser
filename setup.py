36a3c8e549665e50e4fd6ade818439813cba16dd
new-village <kazuki.niimura@gmail.com>
2024-05-16 09:14:06 +0900

    fix required libraries

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='cnparser',
-    version='1.5.0',
+    version='1.5.1',
     author='new-village',
     url='https://github.com/new-village/cnparser',
     description='cnparser is a parser library of Corporate Number Publication Site data.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     license = 'GPLv3+',
-    install_requires=['requests', 'bs4', 'pykakasi', 'kanjize==1.5.0', 'normalize-japanese-addresses==0.0.9'],
+    install_requires=['requests', 'bs4', 'pandas', 'pykakasi', 'kanjize==1.5.0', 'normalize-japanese-addresses==0.0.9'],
     packages=find_packages(),
     package_data={'': ['config/*.json']},
 )
